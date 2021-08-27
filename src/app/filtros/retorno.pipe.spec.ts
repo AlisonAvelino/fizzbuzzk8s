b849f2b58b98c9ccc9c4e2437add586c0a213656
Alison Avelino <alisonavelino3133@gmail.com>
2021-08-27 10:12:26 -0300

    Incluindo os filtros da aplicação

---
+++ src/app/filtros/retorno.pipe.spec.ts
@@ -0,0 +1,16 @@
+import { pipe } from 'rxjs';
+import { RetornoPipe } from './retorno.pipe';
+
+describe('RetornoPipe', () => {
+  const pipe = new RetornoPipe();
+  it('create an instance', () => {
+    const pipe = new RetornoPipe();
+    expect(pipe).toBeTruthy();
+  });
+
+  it('deve exibir mensagem contendo 15 elementos', () => {
+    const resultado = pipe.transform(15);
+    expect(resultado).toBe('Exibindo os primeiros 15 resultados.');
+  });
+
+});
