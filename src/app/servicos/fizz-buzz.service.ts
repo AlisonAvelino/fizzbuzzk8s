8e0c01481e6f079118f4cb8e5a3cdbf7e679237c
sbragan <sbragan@indracompany.com>
2021-08-27 10:08:19 -0300

    Criando servicos para execucao da logica da aplicacao

---
+++ src/app/servicos/fizz-buzz.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FizzBuzzService {
+
+  constructor() { }
+
+  gerar(limite: number): string[] {
+    const fizzBuss: string[] = [];
+    for(let i = 0; i < limite; i++ ){
+      if((i + 1) % 15 === 0) {
+        fizzBuss.push('FizzBuzz')
+      } else if((i + 1) % 3 === 0 ){
+        fizzBuss.push('Fizz')
+      } else if((i + 1) % 5 === 0 ){
+        fizzBuss.push('Buzz')
+      } else {
+        fizzBuss.push((i + 1).toString());
+      }
+    }
+    return fizzBuss;
+  }
+  
+}
